4aa2b69aee0e9182fc48d1bbb8e4bdd8820e8625
Juan Camilo Leal <juancamiloleal07@gmail.com>
2026-06-25 19:12:25 -0500

    feat: Implement user registration with POST request

--- main.js
+++ main.js
@@ -5,3 +5,20 @@ document.addEventListener("product-submit", (event) => {
   items.push(item);
   localStorage.setItem("items", JSON.stringify(items));
 });
+
+const user = {
+  username: "jleal007",
+  password: "112233dfg"
+}
+
+const httpClient = fetch(`${URL_BASE}/user.json`,{
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(user)
+});
+
+const res = httpClient.then(data => data.json());
+res.then(data => console.log).catch(err => console.log)
+
